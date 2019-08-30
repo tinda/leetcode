f5c5065ee69ed79f012b338d46e9ac7c1cc9a61d
tinda <techtinda@hotmail.com>
2019-08-30 16:35:55 +0800

    (+) unit for leetcode

---
+++ src/main/java/Length_of_Last_Word.java
@@ -0,0 +1,6 @@
+public class Length_of_Last_Word {
+
+    public int lengthOfLastWord(String s) {
+        return s.trim().length();
+    }
+}
---
+++ src/test/java/Length_of_Last_WordTest.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * https://leetcode.com/problems/length-of-last-word/#/description
+ * <p>
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ * <p>
+ * If the last word does not exist, return 0.
+ * <p>
+ * Note: A word is defined as a character sequence consists of non-space characters only.
+ * <p>
+ * Example:
+ * <p>
+ * Input: "Hello World"
+ * Output: 5
+ */
+public class Length_of_Last_WordTest {
+
+    @Test
+    public void s_is_bc_should_return_2() {
+        String s = "bc";
+
+        LastWordLengthShouldBe(s, 2);
+    }
+
+    @Test
+    public void s_is_bc_and_3_space_should_return_2() {
+        LastWordLengthShouldBe("bc ", 2);
+    }
+
+    private static void LastWordLengthShouldBe(String s, int expected) {
+        Assert.assertSame(expected, new Length_of_Last_Word().lengthOfLastWord(s));
+    }
+}
