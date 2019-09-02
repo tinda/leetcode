ff3367447bfd1245de328804b86cb54adee228a0
tinda <techtinda@hotmail.com>
2019-09-02 14:46:28 +0800

    (^) Add Two Numbers TDD step 14 重構：Inline Variable 處理，得到最終版本生產代碼

--- src/main/java/Add_Two_Numbers.java
+++ src/main/java/Add_Two_Numbers.java
@@ -65,20 +65,15 @@ public class Add_Two_Numbers {
 
     private ListNode CreateSumNode(ListNode l1, ListNode l2, int carry) {
         if (l1 == null && l2 == null) {
-            if (carry == 0) {
-                return null;
-            }
-
-            return new ListNode(carry);
+            return carry == 0 ? null : new ListNode(carry);
         }
 
         int l1Val = l1 != null ? l1.val : 0;
         int l2Val = l2 != null ? l2.val : 0;
 
         int rootSum = carry + l1Val + l2Val;
-        int rootVal = rootSum % 10;
 
-        ListNode result = new ListNode(rootVal);
+        ListNode result = new ListNode(rootSum % 10);
 
         int carryNext = rootSum >= 10 ? 1 : 0;
         result.next = CreateSumNode(l1.next != null ? l1.next : null, l2.next != null ? l2.next : null, carryNext);
