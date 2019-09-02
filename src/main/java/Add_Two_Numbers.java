f10b05ec674b930ad11f4671644550f5d948db8c
tinda <techtinda@hotmail.com>
2019-09-02 14:44:33 +0800

    (^) Add Two Numbers TDD step 13 重構：以遞迴取代原本的 next 與 next.next 的處理

--- src/main/java/Add_Two_Numbers.java
+++ src/main/java/Add_Two_Numbers.java
@@ -20,6 +20,10 @@ import java.util.List;
 public class Add_Two_Numbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return CreateSumNode(l1, l2, 0);
+    }
+
+    public ListNode addTwoNumbers_old(ListNode l1, ListNode l2) {
 
         int carry_0 = 0;
         int l1Val = l1.val;
@@ -55,6 +59,29 @@ public class Add_Two_Numbers {
                 result.next.next = new ListNode(carry_2 + l1Next_2_Val + l2Next_2_Val);
             }
         }
+
+        return result;
+    }
+
+    private ListNode CreateSumNode(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null) {
+            if (carry == 0) {
+                return null;
+            }
+
+            return new ListNode(carry);
+        }
+
+        int l1Val = l1 != null ? l1.val : 0;
+        int l2Val = l2 != null ? l2.val : 0;
+
+        int rootSum = carry + l1Val + l2Val;
+        int rootVal = rootSum % 10;
+
+        ListNode result = new ListNode(rootVal);
+
+        int carryNext = rootSum >= 10 ? 1 : 0;
+        result.next = CreateSumNode(l1.next != null ? l1.next : null, l2.next != null ? l2.next : null, carryNext);
 
         return result;
     }
