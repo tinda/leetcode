8d67520903b0611415157ea204fedd2e2894b7ec
tinda <techtinda@hotmail.com>
2019-09-03 17:31:51 +0800

    (^) TDD step 2 重構測試代碼:​擷取方法，將 assertion 行為抽取到 AssertMaxProfitShouldBe()

--- src/test/java/Best_Time_to_Buy_and_Sell_StockTest.java
+++ src/test/java/Best_Time_to_Buy_and_Sell_StockTest.java
@@ -8,6 +8,10 @@ public class Best_Time_to_Buy_and_Sell_StockTest {
     @Test
     public void prices_is_5_4_maxProfit_should_be_0() {
         int[] prices = {5, 4};
-        Assert.assertEquals(0, new Best_Time_to_Buy_and_Sell_Stock().maxProfit(prices));
+        AssertMaxProfitShouldBe(prices, 0);
+    }
+
+    private static void AssertMaxProfitShouldBe(int[] prices, int expected) {
+        Assert.assertEquals(expected, new Best_Time_to_Buy_and_Sell_Stock().maxProfit(prices));
     }
 }
