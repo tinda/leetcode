9f9ef5c47531174ebedfc87daacc015a7a71500c
tinda <techtinda@hotmail.com>
2019-10-02 16:23:45 +0800

    (+) 220. Contains Duplicate III TDD step 0 新增 leetcode info and start to TDD

---
+++ src/main/java/Contains_Duplicate_III.java
@@ -0,0 +1,10 @@
+/**
+ * Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+ * https://leetcode.com/problems/contains-duplicate-iii/
+ */
+
+public class Contains_Duplicate_III {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        return false;
+    }
+}
