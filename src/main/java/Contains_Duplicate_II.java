cbd4fc2042c9b40a6d48e8a08cbd84ea2e8b2ab5
tinda <techtinda@hotmail.com>
2019-10-02 16:38:06 +0800

    (+) 219. Contains Duplicate II TDD step 1 第一個紅燈，k = 0，應回傳 false

--- src/main/java/Contains_Duplicate_II.java
+++ src/main/java/Contains_Duplicate_II.java
@@ -1,10 +1,13 @@
 /**
  * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
  * https://leetcode.com/problems/contains-duplicate-ii/
+ * <p>
+ * 題目解釋：給一個 int[] nums ，以及一正整數 k ，如果存在著 i 與 j 使得 nums[i] = nums[j]，且 |i-j|<= k，則回傳 true。若不然，則回傳 false。
  */
 
 public class Contains_Duplicate_II {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (k == 0) return false;
         return false;
     }
 }
--- src/test/java/Contains_Duplicate_IITest.java
+++ src/test/java/Contains_Duplicate_IITest.java
@@ -1,5 +1,17 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Contains_Duplicate_IITest {
+
+    /**
+     * 第一個紅燈，k = 0，應回傳 false。
+     */
+
+    @Test
+    public void Test_k_is_zero_should_return_false() {
+        int[] nums = new int[]{1, 2, 3, 1, 2};
+        int k = 0;
+        Assert.assertFalse(Contains_Duplicate_II.containsNearbyDuplicate(nums, k));
+    }
 
 }
