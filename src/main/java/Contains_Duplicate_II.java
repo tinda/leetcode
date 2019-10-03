44bf4eecd501db52f08d3c4f510b50b8e00b1822
tinda <techtinda@hotmail.com>
2019-10-03 09:52:06 +0800

    (^) 219. Contains Duplicate II TDD step 4 新增一個測試案例，nums = {5,6,5}, k = 2, return true;

--- src/main/java/Contains_Duplicate_II.java
+++ src/main/java/Contains_Duplicate_II.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
@@ -12,9 +15,15 @@ public class Contains_Duplicate_II {
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         if (k == 0) return false;
 
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (!set.add(num)) return true;
+        int windowSize = k + 1;
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("**-> " + i);
+            List<Integer> windowNums = Arrays.stream(nums).skip(i).limit(windowSize).boxed().collect(Collectors.toList());
+            Set<Integer> set = new HashSet<>();
+            for (int num : windowNums) {
+                System.out.println("*-> " + num);
+                if (!set.add(num)) return true;
+            }
         }
 
         return false;
--- src/test/java/Contains_Duplicate_IITest.java
+++ src/test/java/Contains_Duplicate_IITest.java
@@ -19,9 +19,8 @@ public class Contains_Duplicate_IITest {
      */
 
     @Test
-    public void Test_nums_5_5_and_k_is_1_should_return_true()
-    {
-        int[] nums = new int[] { 5, 5 };
+    public void Test_nums_5_5_and_k_is_1_should_return_true() {
+        int[] nums = new int[]{5, 5};
         int k = 1;
         Assert.assertTrue(Contains_Duplicate_II.containsNearbyDuplicate(nums, k));
     }
@@ -30,10 +29,29 @@ public class Contains_Duplicate_IITest {
      * 新增一個測試案例，nums = {5,6}, k = 1, return false;
      */
     @Test
-    public void Test_nums_5_6_and_k_is_1_should_return_false()
-    {
-        int[] nums = new int[] { 5, 6 };
+    public void Test_nums_5_6_and_k_is_1_should_return_false() {
+        int[] nums = new int[]{5, 6};
         int k = 1;
         Assert.assertFalse(Contains_Duplicate_II.containsNearbyDuplicate(nums, k));
+    }
+
+    /**
+     * 新增一個測試案例，nums = {5,6,5}, k = 1, return false;
+     */
+    @Test
+    public void Test_nums_5_6_5_and_k_is_1_should_return_false() {
+        int[] nums = new int[]{5, 6, 5};
+        int k = 1;
+        Assert.assertFalse(Contains_Duplicate_II.containsNearbyDuplicate(nums, k));
+    }
+
+    /**
+     * 新增一個測試案例，nums = {5,6,5}, k = 2, return true;
+     */
+    @Test
+    public void Test_nums_5_6_5_and_k_is_2_should_return_true() {
+        int[] nums = new int[]{5, 6, 5};
+        int k = 2;
+        Assert.assertTrue(Contains_Duplicate_II.containsNearbyDuplicate(nums, k));
     }
 }
