0eee85fe04301fc44558fc1eadd77770a9cc1fc5
tinda <techtinda@hotmail.com>
2019-10-03 11:10:39 +0800

    (^) 1. Two Sum TDD step 4
    Step 4, 使用雙層迴圈直接找到 nums[i] + nums[j] == target 的組合

--- src/main/java/Two_Sum.java
+++ src/main/java/Two_Sum.java
@@ -13,7 +13,14 @@ public class Two_Sum {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        return new int[] { 0, 1 };
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0, 1};
     }
 
     public static int[] twoSum2(int[] nums, int target) {
